1b8685f200bd960abbc4b7487ef97c3f31c99e96
Chris Cue <chris@kotelett.no>
2022-08-23 10:13:16 +0200

    init, pre post scripts for mkdocs

--- bin/mkdocs-build.sh
+++ bin/mkdocs-build.sh
@@ -2,10 +2,28 @@
 
 DOCPATH="${1}"
 
-#if [[ ! -L "ManualsDocs" ]]
-#    ln -s $HOME/Sync/ManualsDocs .
-#fi
+if [[ -f "build-init.sh" ]]; then
+    echo "Running init script."
+    ./build-init.sh
+else
+    echo "No init script exists, continuing."
+fi
 
 while inotifywait --event modify --recursive ${DOCPATH} ; do
+    # execute script before mkdocs build if it exists
+    if [[ -f "build-pre.sh" ]]; then
+        echo "Running pre script."
+        ./build-pre.sh
+    else
+        echo "No pre script exists, continuing."
+    fi
+    ## MKDOCS BUILD
     $HOME/bin/mkdocs-venv/bin/mkdocs build
+    # execute script after mkdocs build if it exists
+    if [[ -f "build-post.sh" ]]; then
+        echo "Running post script."
+        ./build-post.sh
+    else
+        echo "No post script exists, continuing."
+    fi
 done
