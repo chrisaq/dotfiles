17ae08cd0618acf65acae019a973136b4888be8b
Chris A. Q. <chris@kotelett.no>
2024-10-26 22:34:54 +0200

    i3 colors, installer-script
    Signed-off-by: Chris A. Q. <chris@kotelett.no>

--- .local/bin/new-install.sh
+++ .local/bin/new-install.sh
@@ -1,6 +1,23 @@
 #!/bin/env bash
 
 # This script is used to bootstrap an installation on a new machine
+# Place on an machine available on the internet and run using curl -sL https://whatever.com/new-install.sh | bash
 echo "Installing required packages required by further installation scripts"
-pacman -Suy git gpg openssh ansible
+pacman -Suy git gpg openssh ansible unzip
 
+systemctl enable sshd --now
+
+# get ansible
+curl -L https://github.com/your-username/your-repo/archive/refs/heads/main.zip -o dotfiles-master.zip
+unzip dotfiles-master.zip
+cd dotfiles-master/.config/ansible
+
+ansible-galaxy collection install kewlfft.aur
+
+echo "Available playbooks:"
+echo "ansible-playbook playbooks/005-install-basics.yml"
+echo "ansible-playbook playbooks/010-network-wired.yml"
+echo "ansible-playbook playbooks/015-network-wifi.yml"
+echo "ansible-playbook playbooks/020-create-user.yml"
+echo "after reboot:"
+echo "ansible-playbook playbooks/050-user-install-aur.yml"
